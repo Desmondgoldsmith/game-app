38d69bfe4cdc1e8c8e86cbc6cc8588d1deddabd2
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-07-28 03:02:27 +0000

    refactoring

--- src/component/game-grid.tsx
+++ src/component/game-grid.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
-import apiClient from "./services/api-client";
-
-interface GamesProps {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: GamesProps[];
-}
+import { UseGames } from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<GamesProps[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((error) => setError(error.message));
-  });
+  const { games, error } = UseGames();
   return (
     <>
       {error && <p>error: {error}</p>}
---
+++ src/hooks/useGames.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface GamesProps {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: GamesProps[];
+}
+
+export const UseGames = () => {
+  const [games, setGames] = useState<GamesProps[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((error) => setError(error.message));
+  });
+  return { games, error };
+};
