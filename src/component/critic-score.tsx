0029d6bd8250b7fdd9fae3cf754f50a9a0bb3288
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-07-29 09:30:23 +0000

    added critic score

--- src/component/critic-score.tsx
+++ src/component/critic-score.tsx
@@ -3,12 +3,18 @@ interface Props {
 }
 const CriticScore = ({ score }: Props) => {
   const colorDet =
-    score > 80 ? `bg-green-400` : score > 60 ? `bg-blue-400` : `bg-zinc-300`;
+    score >= 90
+      ? `bg-green-400 text-yellow font-bold`
+      : score > 80
+      ? `bg-yellow-400 text-green font-bold`
+      : score > 60
+      ? `bg-blue-400 text-zinc-400 font-bold`
+      : `bg-zinc-300 font-bold`;
 
   return (
     <>
       <div
-        className={`p-1 w-8 h-8 text-center items-center rounded-md ${colorDet}`}
+        className={`p-1 w-8 h-8 text-center items-center  rounded-md ${colorDet}`}
       >
         {score}
       </div>
