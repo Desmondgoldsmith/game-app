d4f95a4525f5393a6859cdc8ad3e921d32113cf8
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-07-31 16:43:11 +0000

    styling genre list component

--- src/component/GenreList.tsx
+++ src/component/GenreList.tsx
@@ -3,11 +3,21 @@ import { UseGenre } from "../hooks/useGenre";
 const GenreList = () => {
   const { data } = UseGenre();
   return (
-    <ul>
+    <>
       {data.map((data) => (
-        <li>{data.name}</li>
+        <div key={data.id} className="space-y-5">
+          <div className="flex space-x-3 p-2  items-center">
+            <img
+              src={data.image_background}
+              className="w-[45px] h-[45px] rounded-lg object-cover border "
+            />
+            <p className="hover: cursor-pointer hover: underline">
+              {data.name}
+            </p>
+          </div>
+        </div>
       ))}
-    </ul>
+    </>
   );
 };
 
--- src/hooks/useGenre.tsx
+++ src/hooks/useGenre.tsx
@@ -3,6 +3,7 @@ import UseData from "./useData";
 export interface GenreProps {
   id: number;
   name: string;
+  image_background: string;
 }
 
 export const UseGenre = () => UseData<GenreProps>("/genres");
