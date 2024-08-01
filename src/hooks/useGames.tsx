0d2b9ace791ec2132ec1e99c5209a48689478ba0
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-08-01 04:10:44 +0000

    fixed some more bugs

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -19,7 +19,6 @@ export interface FetchGamesResponse {
   results: GamesProps[];
 }
 
-<<<<<<< HEAD
 export const UseGames = (gameQuery: GameQuery | null) =>
   UseData<GamesProps>(
     "/games",
@@ -31,14 +30,4 @@ export const UseGames = (gameQuery: GameQuery | null) =>
       },
     },
     [gameQuery]
-=======
-export const UseGames = (
-  selectedGenre: GenreProps | null,
-  selectedPlatform: Platform | null
-) =>
-  UseData<GamesProps>(
-    "/games",
-    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id } },
-    [selectedGenre?.id, selectedPlatform?.id]
->>>>>>> eb89316ca5336b61dd5737058e96e60fcf7db690
   );
