76459dd60209f991613253d9fc481dbfe6fd9d9d
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-08-06 23:45:20 +0000

    set up react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import App from "./App.tsx";
 import "./index.css";
@@ -12,6 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-    <ReactQueryDevtools />
+    {/* <ReactQueryDevtools /> */}
   </React.StrictMode>
 );
