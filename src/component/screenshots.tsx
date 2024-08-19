0a4a94f3521a03e2c0583d8c5a45699429a5eb80
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-08-19 10:07:22 +0000

    chore: fixed some stuff

--- src/component/screenshots.tsx
+++ src/component/screenshots.tsx
@@ -1,24 +1,24 @@
-import useScreenshots from "../hooks/useScreenshots";
+// import useScreenshots from "../hooks/useScreenshots";
 
-interface Props {
-  gameId: number;
-}
+// interface Props {
+//   gameId: number;
+// }
 
-const Screenshots = ({ gameId }: Props) => {
-  const { data, isLoading, error } = useScreenshots(gameId);
-  if (isLoading) return null;
-  if (error) console.error(error);
-  return (
-    <div>
-      {data?.results.map((screenshot) => (
-        <img
-          key={screenshot.id}
-          src={screenshot.image}
-          alt="screenshot image"
-        />
-      ))}
-    </div>
-  );
-};
+// const Screenshots = ({ gameId }: Props) => {
+//   const { data, isLoading, error } = useScreenshots(gameId);
+//   if (isLoading) return null;
+//   if (error) console.error(error);
+//   return (
+//     <div>
+//       {data?.results.map((screenshot) => (
+//         <img
+//           key={screenshot.id}
+//           src={screenshot.image}
+//           alt="screenshot image"
+//         />
+//       ))}
+//     </div>
+//   );
+// };
 
-export default Screenshots;
+// export default Screenshots;
